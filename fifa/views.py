cc18abbd11e684c4bbb8a224b1b825bc1ca6605b
Januario Carreiro <jjc70@duke.edu>
2021-04-13 19:18:40 -0400

    added filters to viewsets

--- fifa/views.py
+++ fifa/views.py
@@ -1,21 +1,29 @@
 import io
 
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
 from rest_framework.decorators import action, parser_classes
+from rest_framework.filters import OrderingFilter, SearchFilter
 from rest_framework.parsers import MultiPartParser
 from rest_framework.response import Response
 
-from fifa.enums import ClubEnum
+from fifa.enums import ClubEnum, PlayerEnum
 from fifa.import_models import ImportModel
-from fifa.models import Player, Club
+from fifa.models import Club, Player
 from fifa.serializers import ClubSerializer, PlayerSerializer
 
 
 class ClubViewSet(viewsets.ModelViewSet):
+    """ API endpoint that allows clubs to be viewed or edited. """
     queryset = Club.objects.all()
-    search_fields = [
-        ClubEnum.NAME.value,
+    filter_backends = [
+        DjangoFilterBackend,
+        SearchFilter,
+        OrderingFilter,
     ]
+    filterset_fields = [e.value for e in ClubEnum]
+    search_fields = [e.value for e in ClubEnum]
+    ordering_fields = [e.value for e in ClubEnum]
 
     def get_serializer_class(self):
         return ClubSerializer
@@ -32,19 +40,16 @@ class ClubViewSet(viewsets.ModelViewSet):
 
 
 class PlayerViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows players to be viewed or edited.
-    """
+    """ API endpoint that allows players to be viewed or edited. """
     queryset = Player.objects.all()
-    filterset_fields = [
-
+    filter_backends = [
+        DjangoFilterBackend,
+        SearchFilter,
+        OrderingFilter,
     ]
-    search_fields = [
-
-    ]
-    ordering_fields = [
-
-    ]
+    filterset_fields = [PlayerEnum.NAME.value]
+    search_fields = [PlayerEnum.NAME.value]
+    ordering_fields = [PlayerEnum.NAME.value]
 
     def get_serializer_class(self):
         return PlayerSerializer
