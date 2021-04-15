3bb6f0389c4190e2e29d2ff37a07356c5448b8e5
Januario Carreiro <jjc70@duke.edu>
2021-04-15 15:20:04 -0400

    modified settings

--- djangoHighlight/settings.py
+++ djangoHighlight/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'django_filters',
     'fifa',
 ]
 
@@ -129,3 +130,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static/'
